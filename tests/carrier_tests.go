856cbc62560929a4beebee74cc539f7749aee132
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-09-04 18:50:25 -0300

    Pendiente TestServer

---
+++ tests/carrier_tests.go
@@ -0,0 +1,15 @@
+package tests
+
+import ("testing")
+
+
+func TestSender(t *testing.T) {}
+
+
+func TestTransfer(t *testing.T) {}
+
+
+func TestReceiver(t *testing.T) {}
+
+
+func TestDirectional(t *testing.T) {}
