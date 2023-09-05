9e06d14aa6fd971c78a8e844b9c86b9ea1ed3e16
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-09-04 21:07:00 -0300

    Features/server 02 (#3)
    * Corrección de imports
    
    * Creación de *Net Init
    
    * Modificación del CallPacket
    
    * Implementación de go func { } ( )

--- server/net.go
+++ server/net.go
@@ -1,11 +1,56 @@
 package server
 
+import (
+	"time"
+	"fmt"
+)
 
 type Net struct {
-
+	directCh 	chan RemoteRespCall
+	quitCh		chan struct{} 
+	NetOptions 		
 }
 
 
 type NetOptions struct {
-	Carrier []Carrier
+	Carriers []Carrier
+}
+
+
+func NewNet(options NetOptions) *Net {
+	return &Net{
+		NetOptions: 	options,
+		directCh: 		make(chan RemoteRespCall),
+		quitCh: 		make(chan struct{}, 1),
+	}
+}
+
+
+func (n *Net) Init() {
+	n.InitCarriers()
+	token := time.NewTicker(10 * time.Second)
+
+	init:
+		for {
+		select {
+		case chn := <- n.directCh:
+			fmt.Println(chn)
+		case <- n.quitCh: 
+			break init
+		case <- token.C:
+			fmt.Println("Initialize...")
+		}
+	}
+
+}
+
+
+func (n *Net) InitCarriers() {
+	for _, nt := range n.NetOptions.Carriers {
+		go func(nt Carrier) {
+			for chn := range nt.Sender() {
+				n.directCh <- chn
+			}
+		} (nt)
+	}
 }
