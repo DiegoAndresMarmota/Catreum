84a5b08b7cc69403e18813af3bf4d63ea2edec00
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-09-04 21:06:33 -0300

    Implementación de go func { } ( )

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"time"
+	"github.com/DiegoAndresMarmota/Catreum/server"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	ntLocal := server.CallPacket("LOCAL")
+	ntRemote := server.CallPacket("REMOTE")
+
+	ntLocal.Networking(ntRemote)
+	ntRemote.Networking(ntLocal)
+
+	go func() {
+		for {
+			ntRemote.Receiver(ntLocal.Direction(), []byte("Running"))
+			time.Sleep(3 * time.Second)
+		}
+	}()
+
+	options := server.NetOptions {
+		Carriers: []server.Carrier{ntLocal},
+	}
+
+	n := server.NewNet(options)
+	n.Init()
 }
